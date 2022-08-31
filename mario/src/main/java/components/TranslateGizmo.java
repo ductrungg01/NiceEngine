5f6b0c3be7de0ce561bdb91cf7b53607f58f91b6
UIT.19520324 <68211021+vuductrung1411@users.noreply.github.com>
2022-08-31 23:52:14 +0700

    Gizmo hover state

--- mario/src/main/java/components/TranslateGizmo.java
+++ mario/src/main/java/components/TranslateGizmo.java
@@ -9,15 +9,17 @@ import org.joml.Vector2f;
 import org.joml.Vector4f;
 
 public class TranslateGizmo extends Component{
-    private Vector4f xAxisColor = new Vector4f(1, 0, 0 , 1);
-    private Vector4f xAxisColorHover = new Vector4f();
-    private Vector4f yAxisColor = new Vector4f(0, 1, 0 , 1);
-    private Vector4f yAxisColorHover = new Vector4f();
+    private Vector4f xAxisColor = new Vector4f(1, 0.3f, 0.3f , 1);
+    private Vector4f xAxisColorHover = new Vector4f(1, 0, 0 , 1);
+    private Vector4f yAxisColor = new Vector4f(0.3f, 1, 0.3f , 1);
+    private Vector4f yAxisColorHover = new Vector4f(0, 1, 0 , 1);
 
     private GameObject xAxisObject;
     private GameObject yAxisObject;
     private SpriteRenderer xAxisSprite;
     private SpriteRenderer yAxisSprite;
+    private int gizmoWidth = 16;
+    private int gizmoHeight = 48;
     private GameObject activeGameObject = null;
 
     private Vector2f xAxisOffset = new Vector2f(64, -5);
@@ -31,6 +33,9 @@ public class TranslateGizmo extends Component{
         this.xAxisSprite = this.xAxisObject.getComponent(SpriteRenderer.class);
         this.yAxisSprite = this.yAxisObject.getComponent(SpriteRenderer.class);
         this.propertiesWindow = propertiesWindow;
+
+        this.xAxisObject.addComponent(new NonPickable());
+        this.yAxisObject.addComponent(new NonPickable());
 
         Window.getScene().addGameObjectToScene(this.xAxisObject);
         Window.getScene().addGameObjectToScene(this.yAxisObject);
@@ -46,22 +51,22 @@ public class TranslateGizmo extends Component{
 
     @Override
     public void update(float dt){
-        if (this.activeGameObject != null){
-            this.xAxisObject.transform.position.set(this.activeGameObject.transform.position);
-            this.yAxisObject.transform.position.set(this.activeGameObject.transform.position);
-            this.xAxisObject.transform.position.add(xAxisOffset);
-            this.yAxisObject.transform.position.add(yAxisOffset);
-        }
-
-        if (MouseListener.isDragging()){
-            System.out.println("Dragging");
-        }
-
         this.activeGameObject = this.propertiesWindow.getActiveGameObject();
         if (this.activeGameObject != null){
             this.setActive();
         } else {
             this.setInactive();
+            return;
+        }
+
+        boolean xAxisHot = checkXHoverState();
+        boolean yAxisHot = checkYHoverState();
+
+        if (this.activeGameObject != null){
+            this.xAxisObject.transform.position.set(this.activeGameObject.transform.position);
+            this.yAxisObject.transform.position.set(this.activeGameObject.transform.position);
+            this.xAxisObject.transform.position.add(xAxisOffset);
+            this.yAxisObject.transform.position.add(yAxisOffset);
         }
     }
 
@@ -74,5 +79,33 @@ public class TranslateGizmo extends Component{
         this.activeGameObject = null;
         this.xAxisSprite.setColor(new Vector4f(0, 0, 0,0));
         this.yAxisSprite.setColor(new Vector4f(0, 0, 0,0));
+    }
+
+    private boolean checkXHoverState(){
+        Vector2f mousePos = new Vector2f(MouseListener.getOrthoX(), MouseListener.getOrthoY());
+        if (mousePos.x <= xAxisObject.transform.position.x &&
+                mousePos.x >= xAxisObject.transform.position.x - gizmoHeight &&
+                mousePos.y >= xAxisObject.transform.position.y &&
+                mousePos.y <= xAxisObject.transform.position.y + gizmoWidth){
+            xAxisSprite.setColor(xAxisColorHover);
+            return true;
+        }
+
+        xAxisSprite.setColor(xAxisColor);
+        return false;
+    }
+
+    private boolean checkYHoverState(){
+        Vector2f mousePos = new Vector2f(MouseListener.getOrthoX(), MouseListener.getOrthoY());
+        if (mousePos.x <= yAxisObject.transform.position.x &&
+                mousePos.x >= yAxisObject.transform.position.x - gizmoWidth &&
+                mousePos.y <= yAxisObject.transform.position.y &&
+                mousePos.y >= yAxisObject.transform.position.y - gizmoHeight){
+            yAxisSprite.setColor(yAxisColorHover);
+            return true;
+        }
+
+        yAxisSprite.setColor(yAxisColor);
+        return false;
     }
 }
