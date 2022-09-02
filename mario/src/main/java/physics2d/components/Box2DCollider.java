ca4a7bf4fe95fc7205d7a20635d4fabfb715f21c
UIT.19520324 <68211021+vuductrung1411@users.noreply.github.com>
2022-09-02 22:54:04 +0700

    Initiate Physics2D

---
+++ mario/src/main/java/physics2d/components/Box2DCollider.java
@@ -0,0 +1,18 @@
+package physics2d.components;
+
+import components.Component;
+import org.joml.Vector2f;
+import org.joml.Vector4f;
+
+public class Box2DCollider extends Component {
+    private Vector2f halfSize = new Vector2f(1);
+
+
+    public Vector2f getHalfSize() {
+        return halfSize;
+    }
+
+    public void setHalfSize(Vector2f halfSize) {
+        this.halfSize = halfSize;
+    }
+}
---
+++ mario/src/main/java/physics2d/components/CircleCollider.java
@@ -0,0 +1,16 @@
+package physics2d.components;
+
+import components.Component;
+
+public class CircleCollider extends Component {
+    private float radius = 1.0f;
+
+
+    public float getRadius() {
+        return radius;
+    }
+
+    public void setRadius(float radius) {
+        this.radius = radius;
+    }
+}
