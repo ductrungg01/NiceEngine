1e68c4a9d5649b2eb326cecae91b069c841c6e6a
Vũ Đức Trung <vuductrung14112001@gmail.com>
2022-11-09 22:49:25 +0700

    Creating an AssetPool for Sounds

--- mario/src/main/java/util/AssetPool.java
+++ mario/src/main/java/util/AssetPool.java
@@ -1,11 +1,13 @@
 package util;
 
 import components.Spritesheet;
+import jade.Sound;
 import org.lwjgl.system.CallbackI;
 import renderer.Shader;
 import renderer.Texture;
 
 import java.io.File;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,6 +15,7 @@ public class AssetPool {
     private static Map<String, Shader> shaders = new HashMap<>();
     private static Map<String, Texture> textures = new HashMap<>();
     private static Map<String, Spritesheet> spritesheets = new HashMap<>();
+    private static Map<String, Sound> sounds = new HashMap<>();
 
     public static Shader getShader(String resourceName){
         File file = new File(resourceName);
@@ -52,5 +55,31 @@ public class AssetPool {
         }
 
         return AssetPool.spritesheets.getOrDefault(file.getAbsolutePath(), null);
+    }
+
+    public static Collection<Sound> getAllSounds(){
+        return sounds.values();
+    }
+
+    public static Sound getSound(String soundFile){
+        File file = new File(soundFile);
+        if (sounds.containsKey(file.getAbsolutePath())){
+            return sounds.get(file.getAbsolutePath());
+        } else {
+            assert false : "Sound file not added '" + soundFile + "'";
+        }
+
+        return null;
+    }
+    public static Sound addSound(String soundFile, boolean loops){
+        File file = new File(soundFile);
+        if (sounds.containsKey(file.getAbsolutePath())){
+            return sounds.get(file.getAbsolutePath());
+        } else {
+            Sound sound = new Sound(file.getAbsolutePath(), loops);
+            AssetPool.sounds.put(file.getAbsolutePath(), sound);
+
+            return sound;
+        }
     }
 }
