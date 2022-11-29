5708e58cf8d830f3d71f59a078930e5abad7b769
Vũ Đức Trung <vuductrung14112001@gmail.com>
2022-11-29 13:28:49 +0700

    [Coding] Creating MushroomAI

--- mario/src/main/java/components/MushroomAI.java
+++ mario/src/main/java/components/MushroomAI.java
@@ -1,4 +1,28 @@
 package components;
 
-public class MushroomAI {
+import org.joml.Vector2f;
+import physics2d.components.RigidBody2D;
+import util.AssetPool;
+
+public class MushroomAI extends Component{
+    private transient boolean goingRight = true;
+    private transient RigidBody2D rb;
+    private transient Vector2f speed = new Vector2f(1.0f, 0.0f);
+    private transient float maxSpeed = 0.8f;
+    private transient boolean hitPlayer = false;
+
+    @Override
+    public void start(){
+        this.rb = gameObject.getComponent(RigidBody2D.class);
+        AssetPool.getSound("assets/sounds/powerup_appears.ogg");
+    }
+
+    @Override
+    public void update(float dt){
+        if (goingRight && Math.abs(rb.getVelocity().x) < maxSpeed){
+            rb.addVelocity(speed);
+        } else if (!goingRight && Math.abs(rb.getVelocity().x) < maxSpeed){
+            rb.addVelocity(new Vector2f(-speed.x, speed.y));
+        }
+    }
 }
