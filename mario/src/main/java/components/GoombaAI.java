be528041c22cdb67b552a8680fbc4b2d5193dea5
Vũ Đức Trung <vuductrung14112001@gmail.com>
2022-12-14 04:22:37 +0700

    [Coding] Giving the goomba custom physiccs

--- mario/src/main/java/components/GoombaAI.java
+++ mario/src/main/java/components/GoombaAI.java
@@ -1,4 +1,63 @@
 package components;
 
+import jade.Camera;
+import jade.Window;
+import org.joml.Vector2f;
+import org.lwjgl.system.CallbackI;
+import physics2d.Physics2D;
+import physics2d.components.RigidBody2D;
+
 public class GoombaAI extends Component {
+
+    private transient boolean goingRight = false;
+    private transient RigidBody2D rb;
+    private transient float walkSpeed = 0.6f;
+    private transient Vector2f velocity = new Vector2f();
+    private transient Vector2f acceleration = new Vector2f();
+    private transient Vector2f terminalVelocity = new Vector2f();
+    private transient boolean onGround = false;
+    private transient boolean isDead = false;
+    private transient float timeToKill = 0.5f;
+    private transient StateMachine stateMachine;
+
+    @Override
+    public void start(){
+        this.stateMachine = gameObject.getComponent(StateMachine.class);
+        this.rb = gameObject.getComponent(RigidBody2D.class);
+        this.acceleration.y = Window.getPhysics().getGravity().y * 0.7f;
+    }
+
+    @Override
+    public void update(float dt){
+        Camera camera = Window.getScene().camera();
+        if (this.gameObject.transform.position.x >
+                camera.position.x + camera.getProjectionSize().x * camera.getZoom()){
+            return;
+        }
+
+        if (goingRight){
+            velocity.x = walkSpeed;
+        } else {
+            velocity.x = -walkSpeed;
+        }
+
+        checkOnGround();
+        if (onGround){
+            this.acceleration.y = 0;
+            this.velocity.y = 0;
+        } else {
+            this.acceleration.y = Window.getPhysics().getGravity().y * 0.7f;
+        }
+
+        this.velocity.y += this.acceleration.y * dt;
+        this.velocity.y = Math.max(Math.min(this.velocity.y, this.terminalVelocity.y),
+                                -terminalVelocity.y);
+        this.rb.setVelocity(velocity);
+    }
+
+    public void checkOnGround(){
+        float innerPlayerWidth = 0.25f * 0.7f;
+        float yVal = -0.14f;
+        onGround = Physics2D.checkOnGround(this.gameObject, innerPlayerWidth, yVal);
+    }
 }
