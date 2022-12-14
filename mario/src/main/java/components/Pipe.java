26611a838ec8079245fca3e1e4df24328c59b8ba
Vũ Đức Trung <vuductrung14112001@gmail.com>
2022-12-14 15:30:57 +0700

    [Coding] creating pipe component

---
+++ mario/src/main/java/components/Pipe.java
@@ -0,0 +1,12 @@
+package components;
+
+import jade.Direction;
+
+public class Pipe extends Component{
+
+    private Direction direction;
+
+    public Pipe(Direction direction){
+        this.direction = direction;
+    }
+}
