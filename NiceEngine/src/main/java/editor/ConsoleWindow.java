83503b605d34fd9f9588f0c30f3791cd16dcb9a3
Vũ Đức Trung <vuductrung14112001@gmail.com>
2023-05-03 14:22:36 +0700

    Make Console window is easy to see full of logs

--- NiceEngine/src/main/java/editor/ConsoleWindow.java
+++ NiceEngine/src/main/java/editor/ConsoleWindow.java
@@ -4,10 +4,13 @@ import editor.uihelper.ButtonColor;
 import imgui.ImGui;
 import imgui.ImVec2;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static editor.uihelper.NiceImGui.NiceButton;
+import static util.FileUtils.getAllFilesWithExtensions;
 
 public class ConsoleWindow {
     //region Singleton
@@ -35,7 +38,7 @@ public class ConsoleWindow {
             debugLogs.clear();
         }
 
-        ImGui.beginChild("consoleItem", ImGui.getContentRegionMaxX(), ImGui.getContentRegionMaxY(), false);
+        ImGui.beginChild("consoleItem", ImGui.getContentRegionMaxX() - 50, ImGui.getContentRegionMaxY() - 100, false);
 
         for (int i = 0; i < debugLogs.size(); i++) {
             ImGui.text(debugLogs.get(i));
