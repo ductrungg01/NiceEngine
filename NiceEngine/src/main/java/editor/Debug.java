d3e12ced4a0887c1b30ded4c808e43a02dccc505
Vũ Đức Trung <vuductrung14112001@gmail.com>
2023-05-12 02:19:21 +0700

    Dynamic type for Debug.Log

--- NiceEngine/src/main/java/editor/Debug.java
+++ NiceEngine/src/main/java/editor/Debug.java
@@ -1,10 +1,6 @@
 package editor;
 
 public class Debug {
-    public static void Log(String s) {
-        ConsoleWindow.getInstance().debugLogs.add(s);
-    }
-
     public static <T> void Log(T t) {
         ConsoleWindow.getInstance().debugLogs.add(t.toString());
     }
