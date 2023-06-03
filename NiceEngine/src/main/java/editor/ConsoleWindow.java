6b1c44e4d6ad6a06ae70d475095b61a43bf807ec
Vũ Đức Trung <vuductrung14112001@gmail.com>
2023-06-03 15:09:54 +0700

    [Refactor] Remove head of debugLogs to improve performance

--- NiceEngine/src/main/java/editor/ConsoleWindow.java
+++ NiceEngine/src/main/java/editor/ConsoleWindow.java
@@ -2,14 +2,9 @@ package editor;
 
 import editor.uihelper.ButtonColor;
 import imgui.ImGui;
-import org.lwjgl.system.CallbackI;
-import system.MouseListener;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static editor.NiceImGui.drawButton;
-import static util.FileUtils.getAllFilesWithExtensions;
 
 public class ConsoleWindow {
     //region Singleton
@@ -33,7 +28,7 @@ public class ConsoleWindow {
     private boolean isRemoved = false;
     static boolean scrollToBottom = true;
     static boolean firstFrame = true;
-    
+
     public void imgui() {
         ImGui.begin("Console");
 
@@ -41,10 +36,11 @@ public class ConsoleWindow {
             debugLogs.clear();
         }
 
-        ImGui.beginChild("consoleItem", 0, 0, true);
+        ImGui.beginChild("consoleItems", 0, 0, true);
 
         if (debugLogs.size() > MAX_DEBUGLOG_SIZE) {
-            removeOldValue();
+            debugLogs.remove(0);
+            isRemoved = true;
         }
 
         if (isRemoved) {
@@ -69,16 +65,5 @@ public class ConsoleWindow {
         firstFrame = false;
 
         ImGui.end();
-    }
-
-    // TODO: this is temporary method, we need to find better solution, we will implement LinkedList instead of List in the future
-    private void removeOldValue() {
-        int n = debugLogs.size();
-        for (int i = 0; i < n - 1; i++) {
-            debugLogs.set(i, debugLogs.get(i + 1));
-        }
-
-        debugLogs.remove(n - 1);
-        isRemoved = true;
     }
 }
