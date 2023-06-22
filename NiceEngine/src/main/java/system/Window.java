21c7db5309ed2a15931b2a56606272c0d1f99ef9
Vũ Đức Trung <vuductrung14112001@gmail.com>
2023-06-22 18:22:34 +0700

    [Fixing] Don't save when Playing

--- NiceEngine/src/main/java/system/Window.java
+++ NiceEngine/src/main/java/system/Window.java
@@ -374,7 +374,8 @@ public class Window implements Observer {
                 Window.getScene().camera().position = oldEditorCameraPos;
                 break;
             case SaveLevel:
-                currentScene.save(true);
+                if (!this.runtimePlaying)
+                    currentScene.save(true);
             case LoadLevel:
                 Window.changeScene(new EditorSceneInitializer());
                 break;
