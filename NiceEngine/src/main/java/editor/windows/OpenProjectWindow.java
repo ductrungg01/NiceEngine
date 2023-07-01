be710030b34a6caba1923fc645b75536977bf695
Vũ Đức Trung <vuductrung14112001@gmail.com>
2023-07-02 02:25:13 +0700

    [Coding] Search project

--- NiceEngine/src/main/java/editor/windows/OpenProjectWindow.java
+++ NiceEngine/src/main/java/editor/windows/OpenProjectWindow.java
@@ -34,12 +34,14 @@ public class OpenProjectWindow {
     private static int projectIconTexId = -1;
     private static boolean allowToCancel = false;
     private static String projectNeedToRename = "";
+    private static String search = "";
 
     public static void open(boolean allowToCancel){
         isOpen = true;
         getListProject();
         projectIconTexId = FileUtils.getIcon(FileUtils.ICON_NAME.PROJECT).getTexId();
         OpenProjectWindow.allowToCancel = allowToCancel;
+        search = "";
     }
 
     public static void imgui(){
@@ -57,141 +59,148 @@ public class OpenProjectWindow {
         if (ImGui.beginPopupModal("Open project", ImGuiWindowFlags.NoMove | ImGuiWindowFlags.NoResize)){
             ImGui.text("Select a project below or");
             ImGui.sameLine();
-            if (NiceImGui.drawButton("Create new project", new ButtonColor(COLOR_DarkGreen, COLOR_Green, COLOR_DarkGreen), new Vector2f(200, 25))) {
+            if (NiceImGui.drawButton("Create new project", new ButtonColor(COLOR_DarkGreen, COLOR_Green, COLOR_DarkGreen), new Vector2f(200, 30))) {
                 CreateNewProjectWindow.open(true);
                 isOpen = false;
             }
+            ImGui.separator();
+
+
+            search = NiceImGui.inputText("Search: ", search, "Enter project name to search", 0, "SearchProjectFromOpenProjectWindow");
 
             Vector4f textColor = Settings.NAME_COLOR;
-            ImGui.textColored(textColor.x, textColor.y, textColor.z, textColor.w, "Project list");
+            ImGui.textColored(textColor.x, textColor.y, textColor.z, textColor.w, "PROJECT LIST");
 
-            ImGui.beginChild("ProjectList", ImGui.getContentRegionMaxX() * 0.99f, ImGui.getContentRegionMaxY() * 0.8f, true);
+            ImGui.beginChild("ProjectList", ImGui.getContentRegionMaxX() * 0.99f, ImGui.getContentRegionMaxY() * 0.75f, true);
             for (int i = 0; i < projects.size(); i++){
                 String p = projects.get(i);
-                boolean needToBreakFlag = false;
 
-                ImGui.pushID("project" + p);
-                ImGui.image(projectIconTexId, 25f, 25f);
-                ImGui.sameLine();
+                if (search.isEmpty() || p.toLowerCase().contains(search.toLowerCase())) {
+                    boolean needToBreakFlag = false;
+
+                    ImGui.pushID("project" + p);
+                    ImGui.image(projectIconTexId, 25f, 25f);
+                    ImGui.sameLine();
 
 
-                if (p.equals(projectNeedToRename)){
-                    boolean changeCurrentProjectFlag = p.equals(ProjectUtils.CURRENT_PROJECT);
+                    if (p.equals(projectNeedToRename)) {
+                        boolean changeCurrentProjectFlag = p.equals(ProjectUtils.CURRENT_PROJECT);
 
-                    String[] newName = NiceImGui.inputTextNoLabel(p);
-                    if (newName[0].equals("true")) {
-                        if (!newName[1].equals(p)) {
-                            if (projects.contains(newName[1])){
-                                JOptionPane.showMessageDialog(null, "The project name '" + newName[1] + "' is existed!",
-                                        "ERROR", JOptionPane.ERROR_MESSAGE);
-                            } else {
-                                File srcFile = new File("data\\" + p);
-                                File desFile = new File("data\\" + newName[1]);
-                                boolean rename = srcFile.renameTo(desFile);
-                                if (!rename) {
-                                    MessageBox.setShowMsb(true);
-                                    MessageBox.setTypeOfMsb(MessageBox.TypeOfMsb.CREATE_FILE_SUCCESS);
-                                    MessageBox.setMsbText("Rename failed");
+                        String[] newName = NiceImGui.inputTextNoLabel(p);
+                        if (newName[0].equals("true")) {
+                            if (!newName[1].equals(p)) {
+                                if (projects.contains(newName[1])) {
+                                    JOptionPane.showMessageDialog(null, "The project name '" + newName[1] + "' is existed!",
+                                            "ERROR", JOptionPane.ERROR_MESSAGE);
                                 } else {
-                                    if (changeCurrentProjectFlag) {
-                                        Window.get().changeCurrentProject(newName[1], false, false);
+                                    File srcFile = new File("data\\" + p);
+                                    File desFile = new File("data\\" + newName[1]);
+                                    boolean rename = srcFile.renameTo(desFile);
+                                    if (!rename) {
+                                        MessageBox.setShowMsb(true);
+                                        MessageBox.setTypeOfMsb(MessageBox.TypeOfMsb.CREATE_FILE_SUCCESS);
+                                        MessageBox.setMsbText("Rename failed");
+                                    } else {
+                                        if (changeCurrentProjectFlag) {
+                                            Window.get().changeCurrentProject(newName[1], false, false);
+                                        }
                                     }
-                                }
 
-                                needToBreakFlag = true;
-                                getListProject();
+                                    needToBreakFlag = true;
+                                    getListProject();
+                                }
                             }
                         }
-                    }
 
-                    if (ImGui.isItemHovered()){
-                        ImGui.beginTooltip();
-                        ImGui.textColored(textColor.x, textColor.y, textColor.z, textColor.w, "Enter to confirm rename");
-                        ImGui.endTooltip();
-                    }
-                } else {
-                    ImGui.button(p);
-
-                    boolean currentProjectIsHovered = false;
-
-                    if (p.equals(ProjectUtils.CURRENT_PROJECT)){
-                        if (ImGui.isItemHovered()) currentProjectIsHovered = true;
-                        ImGui.sameLine();
-                        ImGui.textUnformatted("(This is current project)");
-                    }
-                    if (ImGui.isItemHovered() || currentProjectIsHovered){
-                        ImGui.beginTooltip();
-                        ImGui.textColored(textColor.x, textColor.y, textColor.z, textColor.w, "Double-click to open this project!");
-                        ImGui.text("Right-click to more option");
-                        ImGui.endTooltip();
-
-                        if (ImGui.isMouseDoubleClicked(GLFW.GLFW_MOUSE_BUTTON_LEFT)){
-                            Window.get().changeCurrentProject(p, true, true);
-                            isOpen = false;
+                        if (ImGui.isItemHovered()) {
+                            ImGui.beginTooltip();
+                            ImGui.textColored(textColor.x, textColor.y, textColor.z, textColor.w, "Enter to confirm rename");
+                            ImGui.endTooltip();
                         }
+                    } else {
+                        ImGui.button(p);
 
-                        if (ImGui.isMouseClicked(GLFW_MOUSE_BUTTON_RIGHT)){
-                            ImGui.openPopup("Item popup");
+                        boolean currentProjectIsHovered = false;
+
+                        if (p.equals(ProjectUtils.CURRENT_PROJECT)) {
+                            if (ImGui.isItemHovered()) currentProjectIsHovered = true;
+                            ImGui.sameLine();
+                            ImGui.textUnformatted("(This is current project)");
                         }
-                    }
+                        if (ImGui.isItemHovered() || currentProjectIsHovered) {
+                            ImGui.beginTooltip();
+                            ImGui.textColored(textColor.x, textColor.y, textColor.z, textColor.w, "Double-click to open this project!");
+                            ImGui.text("Right-click to more option");
+                            ImGui.endTooltip();
 
-                    if (ImGui.beginPopup("Item popup")){
-                        if (ImGui.menuItem("Rename project")){
-                            projectNeedToRename = p;
-                        }
-                        if (ImGui.menuItem("Open in Explorer")) {
-                            try {
-                                File file = new File("data\\" + p);
-                                String command = "explorer.exe \"" + file.getAbsolutePath() + "\"";
+                            if (ImGui.isMouseDoubleClicked(GLFW.GLFW_MOUSE_BUTTON_LEFT)) {
+                                Window.get().changeCurrentProject(p, true, true);
+                                isOpen = false;
+                            }
 
-                                Runtime.getRuntime().exec(command);
-                            } catch (IOException e) {
-                                MessageBox.setContext(true, MessageBox.TypeOfMsb.ERROR, "Error! Can't open project");
+                            if (ImGui.isMouseClicked(GLFW_MOUSE_BUTTON_RIGHT)) {
+                                ImGui.openPopup("Item popup");
                             }
                         }
-                        if (ImGui.menuItem("Delete this project")){
-                            int response = JOptionPane.showConfirmDialog(null,
-                                    "DELETE project '" + p + "'?\nProject will be removed permanent\nYou cannot undo this action!",
-                                    "DELETE PROJECT", JOptionPane.YES_NO_OPTION);
-                            if (response == JOptionPane.YES_OPTION) {
-                                if (p.equals(ProjectUtils.CURRENT_PROJECT)){
-                                    Window.get().changeCurrentProject("", false, false);
-                                    OpenProjectWindow.open(false);
-                                }
 
+                        if (ImGui.beginPopup("Item popup")) {
+                            if (ImGui.menuItem("Rename project")) {
+                                projectNeedToRename = p;
+                            }
+                            if (ImGui.menuItem("Open in Explorer")) {
                                 try {
                                     File file = new File("data\\" + p);
-                                    if (file.exists()) {
-                                        String[] command = {"cmd", "/c", "rmdir", "/s", "/q", file.getAbsolutePath()};
-                                        Process process = Runtime.getRuntime().exec(command);
+                                    String command = "explorer.exe \"" + file.getAbsolutePath() + "\"";
 
-                                        int exitCode = process.waitFor();
-                                        if (exitCode == 0) {
-                                            System.out.println("Project '" + p + "' is removed");
-                                        } else {
-                                            System.out.println("Error when remove project '" + p + "'");
-                                        }
-                                    } else {
-                                        System.out.println("Cannot find project '" + p + "'");
-                                    }
+                                    Runtime.getRuntime().exec(command);
                                 } catch (IOException e) {
-                                    System.out.println(e.getMessage());
-                                } catch (InterruptedException e) {
-                                    throw new RuntimeException(e);
+                                    MessageBox.setContext(true, MessageBox.TypeOfMsb.ERROR, "Error! Can't open project");
                                 }
-
-                                needToBreakFlag = true;
-                                getListProject();
                             }
+                            if (ImGui.menuItem("Delete this project")) {
+                                int response = JOptionPane.showConfirmDialog(null,
+                                        "DELETE project '" + p + "'?\nProject will be removed permanent\nYou cannot undo this action!",
+                                        "DELETE PROJECT", JOptionPane.YES_NO_OPTION);
+                                if (response == JOptionPane.YES_OPTION) {
+                                    if (p.equals(ProjectUtils.CURRENT_PROJECT)) {
+                                        Window.get().changeCurrentProject("", false, false);
+                                        OpenProjectWindow.open(false);
+                                    }
+
+                                    try {
+                                        File file = new File("data\\" + p);
+                                        if (file.exists()) {
+                                            String[] command = {"cmd", "/c", "rmdir", "/s", "/q", file.getAbsolutePath()};
+                                            Process process = Runtime.getRuntime().exec(command);
+
+                                            int exitCode = process.waitFor();
+                                            if (exitCode == 0) {
+                                                System.out.println("Project '" + p + "' is removed");
+                                            } else {
+                                                System.out.println("Error when remove project '" + p + "'");
+                                            }
+                                        } else {
+                                            System.out.println("Cannot find project '" + p + "'");
+                                        }
+                                    } catch (IOException e) {
+                                        System.out.println(e.getMessage());
+                                    } catch (InterruptedException e) {
+                                        throw new RuntimeException(e);
+                                    }
+
+                                    needToBreakFlag = true;
+                                    getListProject();
+                                }
+                            }
+
+                            ImGui.endPopup();
                         }
-
-                        ImGui.endPopup();
                     }
+
+                    ImGui.popID();
+
+                    if (needToBreakFlag) break;
                 }
-
-                ImGui.popID();
-
-                if (needToBreakFlag) break;
             }
 
             //check click, check if click outside rename input, use hover any
