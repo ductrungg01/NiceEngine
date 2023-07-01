36c53cf74002da257e1370874c1b0d218c96dae4
Vũ Đức Trung <vuductrung14112001@gmail.com>
2023-07-01 18:57:32 +0700

    [Coding] Don't allow to use Menu bar when the playing scene is running

--- NiceEngine/src/main/java/editor/windows/MenuBar.java
+++ NiceEngine/src/main/java/editor/windows/MenuBar.java
@@ -5,27 +5,35 @@ import imgui.ImGui;
 import observers.EventSystem;
 import observers.events.Event;
 import observers.events.EventType;
+import system.Window;
+
+import javax.swing.*;
 
 public class MenuBar {
     //region Methods
     public void imgui() {
         ImGui.beginMenuBar();
 
-        if (ImGui.beginMenu("File")) {
-            if (ImGui.menuItem("New", "Ctrl+N")){
-                CreateNewProjectWindow.open(false);
-            }
+        if (Window.get().runtimePlaying) {
+            ImGui.text("File");
+        } else {
 
-            if (ImGui.menuItem("Open", "Ctrl+O")) {
-                OpenProjectWindow.open(true);
-            }
+            if (ImGui.beginMenu("File")) {
 
-            if (ImGui.menuItem("Save", "Ctrl+S")) {
-                EventSystem.notify(null, new Event(EventType.SaveLevel));
+                if (ImGui.menuItem("New", "Ctrl+N")) {
+                    CreateNewProjectWindow.open(false);
+                }
+
+                if (ImGui.menuItem("Open", "Ctrl+O")) {
+                    OpenProjectWindow.open(true);
+                }
+
+                if (ImGui.menuItem("Save", "Ctrl+S")) {
+                    EventSystem.notify(null, new Event(EventType.SaveLevel));
+                }
+                ImGui.endMenu();
             }
-            ImGui.endMenu();
         }
-
         ImGui.endMenuBar();
     }
     //endregion
